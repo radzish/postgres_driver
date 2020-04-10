6558b2c13ec3c0149e116ecf83151bff21bf853d
Alexander Radzishevsky <radzish@gmail.com>
2020-04-10 14:35:58 +0300

    db context and transactional gen moved to separate package

--- driver/lib/postgres_driver.dart
+++ driver/lib/postgres_driver.dart
@@ -1,5 +1,3 @@
 library postgres_driver;
 
 export 'src/postgres_driver_impl.dart';
-export 'src/transaction_gen.dart';
-export 'src/db_context.dart';
--- driver_gen/lib/builder.dart
+++
@@ -1,8 +0,0 @@
-library postgres_driver_gen;
-
-import 'package:build/build.dart';
-import 'package:postgres_driver_gen/src/transactional_generator.dart';
-import 'package:source_gen/source_gen.dart';
-
-Builder transactionalGenerator(BuilderOptions options) =>
-    SharedPartBuilder([TransactionalGenerator()], 'transactional_generator');
